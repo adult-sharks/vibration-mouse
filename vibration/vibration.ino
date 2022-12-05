edd7ec1f745fb5a3e0250a2479aa1eecae665eea
moungjae joe <cmj092222@naver.com>
2022-12-05 13:51:49 +0900

    feat: Update pin number ({3, 5, 6} => {3})

--- vibration/vibration.ino
+++ vibration/vibration.ino
@@ -1,45 +1,28 @@
-#define PIN_CNT 3
+#define PIN_NUM 3
 
-int pin_num[3] = {3, 5, 6};
-
-/*
- * 3, 5, 6번 핀에 대응하는 진동 모터 세팅
- */
 void set_vibration_motor() {
-  for (int i = 0; i < PIN_CNT; i++) {
-    pinMode(pin_num[i], OUTPUT);
-  }
+  pinMode(PIN_NUM, OUTPUT);
 }
 
-/*
- * 시리얼 통신 세팅
- */
 void set_serial_communication() {
   Serial.begin(9600);
 }
 
-/*
- * brightness : 밝기값
- * 밝기값을 진동값으로 변경 후 진동 모터에 적용
- */
 void execute_vibration_motor(int brightness) {
   int vibration;
 
   vibration = brightness * 2 > 255 ? 255 : brightness * 2;
-  for (int i = 0; i < PIN_CNT; i++) {
-    analogWrite(pin_num[i], brightness);
-  }
+  analogWrite(PIN_NUM, vibration);
 }
 
-void setup(){
+void setup() {
   set_vibration_motor();
   set_serial_communication();
 }
 
-void loop(){
+void loop() {
   static int brightness;
 
-  // brightness 값을 sandbox.js 으로부터 받아온 경우
   if (Serial.available() > 0) {
     brightness = Serial.read();
     execute_vibration_motor(brightness);
